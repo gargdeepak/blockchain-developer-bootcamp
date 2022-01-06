037e0ae3dcb286e1b3b470827a37769723c1056a
Deepak Garg <deepakgarg.iit@gmail.com>
2022-01-06 12:29:42 +0100

    implement tests to test basic Token contract

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,4 +2,11 @@ pragma solidity ^0.5.0;
 
 contract Token {
     string public name = "Dip"; 
+    string public symbol = "DIP"; 
+    uint256 public decimals = 18; 
+    uint256 public totalSupply ; 
+
+    constructor() public {
+        totalSupply = 1000000 * (10**decimals);
+    }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -5,11 +5,35 @@ require('chai')
     .should()
 
 contract('Token', (accounts) => {
+    let token
+    const name = "Dip"
+    const symbol = "DIP"
+    const decimals = '18'
+    const totalSupply = '1000000000000000000000000'
+
+    beforeEach(async ()=> {
+        token = await Token.new()
+    })
+
     describe('deployment', () =>{
         it('tracks the name', async () => {
-            token = await Token.new()
             const result = await token.name()
             result.should.equal('Dip')
+        })
+
+        it('tracks the symbol', async () => {
+            const result = await token.symbol()
+            result.should.equal('DIP')
+        })
+
+        it('tracks the decimals', async () => {
+            const result = await token.decimals()
+            result.toString().should.equal(decimals)
+        })
+
+        it('tracks the totalSupply', async () => {
+            const result = await token.totalSupply()
+            result.toString().should.equal(totalSupply)
         })
     })
 })
